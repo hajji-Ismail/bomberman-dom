bdc0af086f66024c3ef844d793c816931918323b
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-08-09 17:38:15 +0100

    Refactor movePlayer function to improve code readability and consistency in message formatting

--- server/services/moveplayer.js
+++ server/services/moveplayer.js
@@ -9,53 +9,54 @@ export function movePlayer(data = {}, rooms, stream) {
 
     let step = 0.3 * player.Speed
     const canMove = (cellul) => {
-      
+
         return cellul == 0 || cellul == 11 || cellul == 12 || cellul == 13 || cellul == 14
 
     }
 
 
     let cellul;
-    
+
     switch (data.action) {
 
         case "ArrowRight":
             cellul = map[Math.floor(player.position.y)][Math.floor(player.position.x + step)]
             if (canMove(cellul)) {
-                
-                sendMessages(stream,  {
-                    type :  "canMove",
-                    x : step,
-                    direction :"right"
+
+                sendMessages(stream, {
+                    type: "canMove",
+                    x: step,
+                    direction: "right"
 
                 })
+                player.position.x = player.position.x + step
             }
-
 
 
             break;
         case "ArrowLeft":
             cellul = map[Math.floor(player.position.y)][Math.floor(player.position.x + step)]
             if (canMove(cellul)) {
-                   sendMessages(stream, {
-                    type :  "canMove",
-                    x : step,
-                    direction :"left"
+                sendMessages(stream, {
+                    type: "canMove",
+                    x: step,
+                    direction: "left"
 
                 })
-
+                player.position.x = player.position.x - step
             }
 
             break
         case "ArrowUp":
             cellul = map[Math.floor(player.position.y - step)][Math.floor(player.position.x)]
             if (canMove(cellul)) {
-                   sendMessages(stream, {
-                    type :  "canMove",
-                    y : step,
-                    direction :"up"
+                sendMessages(stream, {
+                    type: "canMove",
+                    y: step,
+                    direction: "up"
 
                 })
+                player.position.y = player.position.y - step
 
             }
 
@@ -63,13 +64,13 @@ export function movePlayer(data = {}, rooms, stream) {
         case "ArrowDown":
             cellul = map[Math.floor(player.position.y + step)][Math.floor(player.position.x)]
             if (canMove(cellul)) {
-                   sendMessages(stream,  {
-                    type :  "canMove",
-                    y : step,
-                    direction :"down"
+                sendMessages(stream, {
+                    type: "canMove",
+                    y: step,
+                    direction: "down"
 
                 })
-
+                player.position.y = player.position.y + step
             }
 
             break
