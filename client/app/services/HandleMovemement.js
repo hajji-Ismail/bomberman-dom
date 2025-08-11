f79e0cbffea18899b44e51feecc0834e10ad25b1
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-08-11 20:35:37 +0100

    handle movement presition plus remove unneseserly code blocks

--- client/app/services/HandleMovemement.js
+++ client/app/services/HandleMovemement.js
@@ -1,27 +1,11 @@
 export function HandleMovement(data = {}, state) {
     const username = data.player.username;
-    let style;
+ let style = `transform: translate(${data.player.position.xstep * 55}px, ${data.player.position.ystep * 55}px)`;
 
 
 
 
 
-    switch (data.direction) {
-        case "right":
-            style = `transform: translate(${data.player.position.xstep * 55}px, ${data.player.position.ystep * 55}px)`;
-            break;
-        case "left":
-            style = `transform: translate(${data.player.position.xstep * 55}px, ${data.player.position.ystep * 55}px)`;
-            break;
-        case "up":
-            style = `transform: translate(${data.player.position.xstep}px, ${data.player.position.ystep* 55}px)`;
-            break;
-        case "down":
-            style = `transform: translate(${data.player.position.xstep}px, ${data.player.position.ystep * 55}px)`;
-            break;
-        default:
-            style = `transform: translate(${data.player.position.xstep}px, ${data.player.position.ystep * 55}px)`;
-    }
 
     // store style per player
     const styles = state.get("playerStyles") || {};
