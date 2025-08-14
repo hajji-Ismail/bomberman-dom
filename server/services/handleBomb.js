c4eb03936d06dcea352da7f65824c7bc046a119f
yelasri <elasriyoussef604@gmail.com>
2025-08-14 14:11:06 +0100

    chwiya dyal tekhmal

--- server/services/handleBomb.js
+++ server/services/handleBomb.js
@@ -1,5 +1,6 @@
 import { broadCastRoom } from "./broadCast.js";
 import { RemovePlayer } from "./RemovePlayer.js";
+import { ResetPositions } from "./setUpPlayers.js";
 export function HandleBomb(player, room) {
   const row = Math.floor(player.position.y);
   const col = Math.floor(player.position.x);
@@ -22,9 +23,9 @@ export function HandleBomb(player, room) {
         if (currentRow === row && currentCol === col) {
           verifyPlayerDamage(room, currentPlayer, idx);
         } else {
-            if (r === currentRow && c === currentCol) {
-              verifyPlayerDamage(room, currentPlayer, idx);
-            }
+          if (r === currentRow && c === currentCol) {
+            verifyPlayerDamage(room, currentPlayer, idx);
+          }
         }
       });
     };
@@ -113,44 +114,10 @@ function verifyPlayerDamage(room, currentPlayer, idx) {
   if (currentPlayer.Lives <= 0) {
     currentPlayer.isLosed = true;
     RemovePlayer(currentPlayer.stream);
-    // broadCastRoom(room, {
-    //     type: "newRoom",
-    //     room
-    // })
     return;
   }
 
   currentPlayer.isDamaged = true;
 
-  switch (idx) {
-    case 0: // Top-left
-      currentPlayer.position = { x: 1.35, y: 1.85, xstep: 0, ystep: 0 };
-      break;
-    case 1: // Top-right
-      currentPlayer.position = {
-        x: mapWidth - 2 + 0.35,
-        y: 1.85,
-        xstep: 0,
-        ystep: 0,
-      };
-      break;
-    case 2:
-      currentPlayer.position = {
-        x: mapWidth - 2 + 0.35,
-        y: mapHeight - 2 + 0.85,
-        xstep: 0,
-        ystep: 0,
-      };
-      break;
-    case 3: // Bottom-left
-      currentPlayer.position = {
-        x: 1.35,
-        y: mapHeight - 2 + 0.85,
-        xstep: 0,
-        ystep: 0,
-      };
-      break;
-    default:
-      break;
-  }
+  ResetPositions(mapWidth, mapHeight, currentPlayer, idx)
 }
--- server/services/setUpPlayers.js
+++ server/services/setUpPlayers.js
@@ -1,8 +1,9 @@
 export function setUpPlayers(room) {
+    const mapWidth = room.map[0].length;
+    const mapHeight = room.map.length;
 
     room.players.forEach((player, i) => {
         player["playerNumber"] = `player${i + 1}`
-
         player.Bombs = 1;
         player.Flames = 1;
         player.Speed = 1;
@@ -10,27 +11,26 @@ export function setUpPlayers(room) {
         player.Lives = 3
         player.isDamaged = false
 
-
-
-        const mapWidth = room.map[0].length;
-        const mapHeight = room.map.length;
-
-        // Set position centered on tile
-        switch (i) {
-            case 0: // Top-left
-                player.position = { x: 1.35, y: 1.85, xstep: 0, ystep: 0 };
-                break;
-            case 1: // Top-right
-                player.position = { x: (mapWidth - 2) + 0.35, y: 1.85, xstep: 0, ystep: 0 };
-                break;
-            case 2:
-                player.position = { x: (mapWidth - 2) + 0.35, y: (mapHeight - 2) + 0.85, xstep: 0, ystep: 0 };
-                break;
-            case 3: // Bottom-left
-                player.position = { x: 1.35, y: (mapHeight - 2) + 0.85, xstep: 0, ystep: 0 };
-                break;
-            default:
-                break;
-        }
+        ResetPositions(mapWidth, mapHeight, player,i)
     });
+}
+
+export function ResetPositions(mapWidth, mapHeight, player, idx) {
+    // Set position centered on tile
+    switch (idx) {
+        case 0: // Top-left
+            player.position = { x: 1.35, y: 1.85, xstep: 0, ystep: 0 };
+            break;
+        case 1: // Top-right
+            player.position = { x: (mapWidth - 2) + 0.35, y: 1.85, xstep: 0, ystep: 0 };
+            break;
+        case 2:
+            player.position = { x: (mapWidth - 2) + 0.35, y: (mapHeight - 2) + 0.85, xstep: 0, ystep: 0 };
+            break;
+        case 3: // Bottom-left
+            player.position = { x: 1.35, y: (mapHeight - 2) + 0.85, xstep: 0, ystep: 0 };
+            break;
+        default:
+            break;
+    }
 }
